f682b11de6d19e2f064a98fcabb3d3249f3429fd
amir kazemi <amirmk837@gmail.com>
2026-02-10 13:21:05 +0330

    Add files via upload

---
+++ object.ts
@@ -0,0 +1,17 @@
+type ob = {name:string , age:number , gender:boolean , city?:string}
+
+let testi : ob = {
+    name:"amir",
+    age:21,
+    gender:true
+}
+
+let testi2 : ob = {
+    name:"sohrab",
+    age:41,
+    gender:true,
+    city:"tehran"
+}
+
+console.log(testi);
+console.log(testi2);
